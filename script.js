907c35c32274a64eb03f3aaaa764b772e877b043
Collin McCartney <collinpmccartney@gmail.com>
2023-07-18 21:47:35 -0400

    Added random generator functions to JavaScript

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Random Generator Functions
+
+function randomLower() {
+ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+ return lowerCase[Math.floor(Math.random() * lowerCase.length)]; 
+}
+
+function randomUpper() {
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+
+function randomNumber() {
+  var number = "0123456789";
+  return number[Math.floor(Math.random() * number.length)];
+}
+
+function randomSymbol() {
+  var symbol = " !'#$%&()*+,-./:;<=>?@[]^_`{|}~"
+  return number[Math.floor(Math.random() * symbol.length)];
+}
