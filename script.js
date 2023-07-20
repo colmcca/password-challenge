d5ba0d01de27b53575b308d7204f38541ccf6a55
Collin McCartney <collinpmccartney@gmail.com>
2023-07-19 21:56:57 -0400

    Added prompts to JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+var passwordLength = window.prompt("Please choose a number between 8 and 128 for your password length.");
+
+if (passwordLength < 8) {
+  alert("Your password lenght must be at least 8 digits.");
+  return "Please try again.";
+}
+
+if (passwordLength > 128) {
+  alert("Your password length cannot exceed 128 digits.");
+  return "Please try again.";
+}
+
+if (isNaN(passwordLength)) {
+  alert("You must type a numerical value.")
+  return "Please try again.";
+}
+
+if (!passwordLength) {
+  return;
+}
+
+if (confirm("Do you want your password to include lowercase letters?")) {
+  randomLower();
+}
+
+if (confirm("Do you want your password to include uppercase letters?")) {
+  randomUpper();
+}
+
+if (confirm("Do you want your password to include numbers?")) {
+  randomNumber();
+}
+
+if (confirm("Do you want your password to include special characters?")) {
+  randomSymbol();
+}
+
+
+  return "This is your password"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
