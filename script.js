67fc6526b14f0de661e1f7f7e47130eb468790b1
Collin McCartney <collinpmccartney@gmail.com>
2023-07-20 17:19:05 -0400

    added code to the generate password function

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * randomArray.length);
+    password = password + randomArray[randomCharacter];
+  }
+  return password;
 }
 // User selection prompts
 function passwordChoice () {
   randomArray = [];
-var passwordLength = window.prompt("Please choose a number between 8 and 128 for your password length.");
+passwordLength = window.prompt("Please choose a number between 8 and 128 for your password length.");
 
 if (passwordLength < 8) {
   alert("Your password length must be at least 8 digits.");
@@ -46,15 +53,15 @@ if (confirm("Do you want your password to include special characters?")) {
 return true;
 }
 
-
-
 // Write password to the #password input
 function writePassword() {
-  passwordChoice();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  var promptChoices = passwordChoice();
+  if(promptChoices) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
 
 }
 
@@ -62,8 +69,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // variables
-
-var passwordLength = 120
+var passwordLength = 50
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
